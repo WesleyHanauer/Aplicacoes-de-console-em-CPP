1c14e1decffba0cb0402fd25955cea9926ef06b0
Wesley Hanauer <whanauerbiz@proton.me>
2023-02-03 08:41:39 -0300

    Melhorias #1

--- Conversor de temperatura/Conversor de temperatura.cpp
+++ Conversor de temperatura/Conversor de temperatura.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-float converter(float num, int tipo1, int tipo2) {
+float converter(float num, std::string de, std::string para) {
     float resultado;
-    if (tipo1 == 1 && tipo2 == 1) {
+    if (de == "fahrenheit" && para == "celsius") {
         resultado = (num - 32) * 5 / 9;
     }
-    else if (tipo1 == 1 && tipo2 == 2) {
+    else if (de == "fahrenheit" && para == "kelvin") {
         resultado = (num - 32) * 5 / 9 + 273.15;
     }
-    else if (tipo1 == 2 && tipo2 == 1) {
+    else if (de == "celsius" && para == "fahrenheit") {
         resultado = (num * 9 / 5) + 32;
     }
-    else if (tipo1 == 2 && tipo2 == 2) {
+    else if (de == "celsius" && para == "kelvin") {
         resultado = num + 273.15;
     }
-    else if (tipo1 == 3 && tipo2 == 2) {
+    else if (de == "kelvin" && para == "celsius") {
         resultado = num - 273.15;
     }
     else {
@@ -25,42 +25,49 @@ float converter(float num, int tipo1, int tipo2) {
 
 int main()
 {
-    float num;
-    int primeiraSelecao;
-    int segundaSelecao;
-    std::string tipo;
-    std::string tipoArr[2];
-    std::cout << "\nSelecione a temperatura inicial.\n\n";
-    std::cout << "1 para Fahrenheit\n";
-    std::cout << "2 para Celsius\n";
-    std::cout << "3 para Kelvin\n";
-    std::cout << "4 para sair\n\n";
-    std::cin >> primeiraSelecao;
+    while (true) {
+        float num;
+        int primeiraSelecao;
+        int segundaSelecao;
+        std::string tipo;
+        std::string tipoArr[2];
+        std::cout << "\nSelecione a temperatura inicial.\n\n";
+        std::cout << "1 Fahrenheit\n";
+        std::cout << "2 Celsius\n";
+        std::cout << "3 Kelvin\n";
+        std::cout << "4 para sair\n\n";
+        std::cin >> primeiraSelecao;
 
-    if (primeiraSelecao == 1) {
-        tipo = "fahrenheit";
-        tipoArr[0] = "Celsius";
-        tipoArr[1] = "Kelvin";
-    }
-    else if (primeiraSelecao == 2) {
-        tipo = "celsius";
-        tipoArr[0] = "Fahrenheit";
-        tipoArr[1] = "Kelvin";
-    }
-    else if (primeiraSelecao == 3){
-        tipo = "kelvin";
-        tipoArr[0] = "Fahrenheit";
-        tipoArr[1] = "Celsius";
-    }
-    else {
-        return 0;
-    }
+        if (primeiraSelecao == 1) {
+            tipo = "fahrenheit";
+            tipoArr[0] = "celsius";
+            tipoArr[1] = "kelvin";
+        }
+        else if (primeiraSelecao == 2) {
+            tipo = "celsius";
+            tipoArr[0] = "fahrenheit";
+            tipoArr[1] = "kelvin";
+        }
+        else if (primeiraSelecao == 3) {
+            tipo = "kelvin";
+            tipoArr[0] = "fahrenheit";
+            tipoArr[1] = "celsius";
+        }
+        else {
+            return 0;
+        }
 
-    std::cout << "\nPara qual temperatura deseja converter?\n";
-    std::cout << "1 para " << tipoArr[0] << "\n";
-    std::cout << "2 para " << tipoArr[1] << "\n\n";
-    std::cin >> segundaSelecao;
-    std::cout << "\nInforme os graus em " << tipo << ": \n\n";
-    std::cin >> num;
-    std::cout << "\n" << converter(num, primeiraSelecao, segundaSelecao);
+        std::cout << "\nPara qual temperatura deseja converter?\n";
+        std::cout << "1 para " << tipoArr[0] << "\n";
+        std::cout << "2 para " << tipoArr[1] << "\n\n";
+        std::cin >> segundaSelecao;
+        std::cout << "\nInforme os graus em " << tipo << ": \n\n";
+        std::cin >> num;
+        if (segundaSelecao == 1) {
+            std::cout << "\n" << converter(num, tipo, tipoArr[0]) << "\n";
+        }
+        else {
+            std::cout << "\n" << converter(num, tipo, tipoArr[1]) << "\n";
+        }
+    }
 }
